983c1ee8cd04211cd41b0a09778a9cffbf4c8264
tahaltuntas <93127440+tahaltuntas@users.noreply.github.com>
2021-12-29 02:54:38 +0300

    materyal offset değerleri kullanarak arka plan sonsuz yapıldı

--- Assets/Scripts/OyunKontrol.cs
+++ Assets/Scripts/OyunKontrol.cs
@@ -16,7 +16,7 @@ public class OyunKontrol : MonoBehaviour
     [SerializeField]
     int zorluk = 1;
     [SerializeField]
-    int carpan = 4;
+    int carpan = 3;
 
     // Start is called before the first frame update
     void Start()
